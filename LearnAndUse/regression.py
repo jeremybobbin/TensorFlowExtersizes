8b461ca1d8e5f68a7d3bdb978927159768b719c5
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-24 13:26:39 -0700

    python boilerplate

---
+++ LearnAndUse/regression.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from tensorflow import keras
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
