8ebba618e0541880a49075148166ad5874219fdf
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-24 15:08:09 -0700

    pipe png to feh like normal person

--- LearnAndUse/regression.py
+++ LearnAndUse/regression.py
@@ -3,7 +3,132 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from tensorflow import keras
+from tensorflow.keras import layers
 
-import tensorflow as tf
-import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+import io
+import sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import subprocess
+
+
+def norm(x):
+  return (x - train_stats['mean']) / train_stats['std']
+
+
+def build_model():
+  model = keras.Sequential([
+    layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation=tf.nn.relu),
+    layers.Dense(1)
+  ])
+  optimizer = tf.keras.optimizers.RMSprop(0.001)
+  model.compile(loss='mean_squared_error',
+                optimizer=optimizer,
+                metrics=['mean_absolute_error', 'mean_squared_error'])
+  return model
+
+def plot_history(history):
+  hist = pd.DataFrame(history.history)
+  hist['epoch'] = history.epoch
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Abs Error [MPG]')
+  plt.plot(hist['epoch'], hist['mean_absolute_error'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
+           label = 'Val Error')
+  plt.ylim([0,5])
+  plt.legend()
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Square Error [$MPG^2$]')
+  plt.plot(hist['epoch'], hist['mean_squared_error'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mean_squared_error'],
+           label = 'Val Error')
+  plt.ylim([0,20])
+  plt.legend()
+  plt.show()
+
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
+
+def view_plot(plt, viewer='feh'):
+    child = subprocess.Popen([viewer, '-'], stdin=subprocess.PIPE)
+    plt.savefig(child.stdin, format='png')
+    return child
+
+
+dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=" ", skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+
+dataset = dataset.dropna()
+origin = dataset.pop("Origin")
+
+dataset['USA'] = (origin == 1)*1.0
+dataset['Europe'] = (origin == 2)*1.0
+dataset['Japan'] = (origin == 3)*1.0
+
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+
+train_labels = train_dataset.pop("MPG")
+test_labels = test_dataset.pop("MPG")
+
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+
+model = build_model()
+example_batch = normed_train_data[:10]
+example_result = model.predict(example_batch)
+
+
+EPOCHS = 1000
+
+
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+history = model.fit(normed_train_data, train_labels, epochs=EPOCHS,
+                    validation_split = 0.2, verbose=0, callbacks=[early_stop])
+
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=0)
+
+test_predictions = model.predict(normed_test_data).flatten()
+
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+plt.axis('equal')
+plt.axis('square')
+plt.xlim([0,plt.xlim()[1]])
+plt.ylim([0,plt.ylim()[1]])
+_ = plt.plot([-100, 100], [-100, 100])
+
+error = test_predictions - test_labels
+plt.hist(error, bins = 25)
+plt.xlabel("Prediction Error [MPG]")
+_ = plt.ylabel("Count")
+
+view_plot(plt)
