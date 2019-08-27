3fafebf4fc644a8e5a1cd47abb5f3591c1dfe984
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-27 08:43:37 -0700

    expressive python iterations

--- ResearchAndExperimentation/eager.py
+++ ResearchAndExperimentation/eager.py
@@ -38,28 +38,22 @@ if tf.test.is_gpu_available():
     time_matmul(x)
 
 
-ds_tensors = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
+
+ds_tensors = tf.data.Dataset.from_tensor_slices([i for i in range(20)])
 
 _, filename = tempfile.mkstemp()
 
 with open(filename, 'w') as f:
-  f.write("""Line 1
-Line 2
-Line 3
-Line 4
-Line 5
-Line 6
-Line 7
-Line 8
-Line 9
-Line 0
-  """)
+    buf = ""
+    for i in range(20):
+        buf += "Line {}\n".format(i)
+    f.write(buf)
 
 ds_file = tf.data.TextLineDataset(filename)
 
-ds_tensors = ds_tensors.map(tf.square).shuffle(2).batch(2)
+ds_tensors = ds_tensors.map(tf.square).batch(3)
 
-ds_file = ds_file.batch(2)
+ds_file = ds_file.batch(3)
 
 print('Elements of ds_tensors:')
 for x in ds_tensors:
