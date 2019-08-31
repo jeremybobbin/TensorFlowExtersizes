3ce733d10bea0cbf451977d75d41e30c016b656d
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-31 13:02:27 -0700

    WALK THROUGH

--- ResearchAndExperimentation/walkthrough.py
+++ ResearchAndExperimentation/walkthrough.py
@@ -7,19 +7,31 @@ import os
 import subprocess
 
 import tensorflow as tf
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import numpy as np
+
+from tensorflow import contrib
+eager = contrib.eager
 
 tf.compat.v1.enable_eager_execution()
 
-def view_plot(plt, viewer='feh'):
+def view_plot(plot, viewer='feh'):
     child = subprocess.Popen([viewer, '-'], stdin=subprocess.PIPE)
-    plt.savefig(child.stdin, format='png')
+    plot.savefig(child.stdin, format='png')
     return child
 
 def pack_features_vector(features, labels):
   features = tf.stack(list(features.values()), axis=1)
   return features, labels
+
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+def loss(model, x, y):
+  y_ = model(x)
+  return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
 
 
 train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
@@ -45,16 +57,101 @@ train_dataset = tf.contrib.data.make_csv_dataset(
     label_name=label_name,
     num_epochs=1)
 
-features, labels = next(iter(train_dataset))
-# features [[120], [4], [setosa], [versicolor]]
-# labels [Enum species]
 
-for value in features.values():
-    print("Value: {}".format(value))
 
 train_dataset = train_dataset.map(pack_features_vector)
 
-print(features)
-print(labels)
+features, labels = next(iter(train_dataset))
 
-# left off at https://www.tensorflow.org/tutorials/eager/custom_training_walkthrough#select_the_model
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+  tf.keras.layers.Dense(10, activation=tf.nn.relu),
+  tf.keras.layers.Dense(3)
+])
+
+
+
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+global_step = tf.Variable(0)
+
+loss_value, grads = grad(model, features, labels)
+
+print("Step: {}, Initial Loss: {}".format(global_step.numpy(),
+                                          loss_value.numpy()))
+
+optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step)
+
+print("\n\nStep: {}\nLoss: {}\n\n".format (global_step.numpy(), loss(model, features, labels).numpy()))
+
+train_loss_results = []
+train_accuracy_results = []
+
+epochs = 201
+
+for epoch in range(epochs):
+    loss_average = eager.metrics.Mean()
+    accuracy = eager.metrics.Accuracy()
+
+    for x, y in train_dataset:
+        loss_value, grads = grad(model, x, y)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step)
+
+
+        loss_average(loss_value)
+        max = tf.argmax(model(x), axis=1, output_type=tf.int32)
+        accuracy(max, y)
+        
+    train_loss_results.append(loss_average.result())
+    train_accuracy_results.append(accuracy.result())
+
+figure, axes = plot.subplots(2, sharex=True, figsize=(12, 8))
+figure.suptitle('Training Metrics')
+
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+
+view_plot(plot)
+
+test_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
+
+test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                  origin=test_url)
+
+test_dataset = tf.contrib.data.make_csv_dataset(
+        test_fp,
+        batch_size,
+        column_names=column_names,
+        label_name='species',
+        num_epochs=1,
+        shuffle=False
+    ).map(pack_features_vector)
+
+
+test_accuracy = eager.metrics.Accuracy()
+
+for (x, y) in test_dataset:
+    logits = model(x)
+    prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
+    test_accuracy(prediction, y)
+
+data = tf.convert_to_tensor([
+    [5.1, 3.3, 1.7, 0.5],
+    [5.9, 3.0, 4.2, 1.5],
+    [6.9, 3.1, 5.4, 2.1]
+])
+
+
+prediction = model(data)
+
+for i, logits in enumerate(prediction):
+    index = tf.argmax(logits).numpy()
+    p = tf.nn.softmax(logits)[index]
+    name = class_names[index]
+    print("Example: {}".format(i))
+    print("Prediction: {} ({:4.1f}%)".format(name, 100 * p))
